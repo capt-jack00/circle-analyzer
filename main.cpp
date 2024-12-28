db49fc7300fd14016261caf03225f3299b4827f2
dbednorz@tuta.io <dbednorz@tuta.io>
2024-12-28 15:30:48 +0100

    Added function that calculates distance between point and circle

--- main.cpp
+++ main.cpp
@@ -3,11 +3,12 @@
 
 using namespace std;
 
-int xc;
-int yc;
-int xp;        
-int yp;
-int rad;
+double xc;
+double yc;
+double xp;        
+double yp;
+double rad;
+double ptCirDist;
 
 void circleEquationDisplay();
 
@@ -19,12 +20,35 @@ int main(){
     cin >> yc;
     cout << "Please enter a radius of a circle: ";
     cin >> rad;
+    cout << "Please enter an X coordinate of point A";
+    cin >> xp;
+    cout << "Please enter an Y coordinate of point A";
+    cin >> yp;
 
-    circleEquationDisplay();
+    //circleEquationDisplay();
 
     return 0;
 }
 
 void circleEquationDisplay(){
     cout << "The equation of this circle is: (x - " << xc << ")^2 + (y - " << yc << ")^2 = " << pow(rad, 2) << endl;
+}
+
+void pointAndCirleDist(){
+    double x = pow(xp - xc, 2);
+    double y = pow(yp - yc, 2);
+    ptCirDist = sqrt(x + y);
+
+    if(ptCirDist > rad){
+        cout << "Point A is outside the circle" << endl;
+    }
+    else if(ptCirDist = rad){
+        cout << "Point A is on the circle" << endl;
+    }
+    else if(ptCirDist < rad){
+        cout << "Point A is inside the circle" << endl; 
+    }
+    else{
+        cout << "Something went wrong" << endl;
+    }
 }
